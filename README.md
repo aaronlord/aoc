22233febd1366616567350198846173c7b614ed6
Aaron Lord <hire@aaron.codes>
2023-12-29 23:49:47 +0000

    Day 7, Part 1

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 - Day 4 [Part 1](https://github.com/aaronlord/aoc/blob/88b8066e6e427586aba31436ac9aa8faa1414f37/app/Console/Commands/Day4Command.php), [Part 2](app/Console/Commands/Day4Command.php)
 - Day 5 [Part 1](https://github.com/aaronlord/aoc/blob/ccbc964f14ebe2bf2b1d83bd580553be46fffdd1/app/Console/Commands/Day5Command.php), [Part 2](app/Console/Commands/Day5Command.php)
 - Day 6 [Part 1](https://github.com/aaronlord/aoc/blob/f37a4a5119c4da68b4241b20140613eea9ab52d6/app/Console/Commands/Day6Command.php), [Part 2](app/Console/Commands/Day6Command.php)
+- Day 7 [Part 1](app/Console/Commands/Day7Command.php)
---
+++ app/Console/Commands/Day7Command.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Console\Commands\Day7\Hand;
+use Illuminate\Console\Command;
+use Illuminate\Contracts\Filesystem\Filesystem;
+
+class Day7Command extends Command
+{
+    protected $signature = 'day:7 {--test}';
+
+    protected $description = 'https://adventofcode.com/2023/day/7';
+
+    public function handle(Filesystem $fs): void
+    {
+        $contents = $fs->get($this->option('test') ? 'day7.test.txt' : 'day7.txt');
+        $contents = explode("\n", $contents);
+
+        $total = collect($contents)
+            ->reject(fn (string $line): bool => empty($line))
+            ->map(Hand::fromString(...))
+            ->sort(fn (Hand $a, Hand $b): int => $a->beats($b) ? 1 : -1)
+            ->values()
+            ->reduce($this->winnings(...), 0);
+
+        $this->line($total);
+    }
+
+    private function winnings(int $carry, Hand $hand, int $index): int
+    {
+        return $carry + ($index + 1) * $hand->bid;
+    }
+}
+
+namespace App\Console\Commands\Day7;
+
+class Hand
+{
+    const FIVE_OF_A_KIND = 6;
+    const FOUR_OF_A_KIND = 5;
+    const FULL_HOUSE = 4;
+    const THREE_OF_A_KIND = 3;
+    const TWO_PAIR = 2;
+    const ONE_PAIR = 1;
+    const HIGH_CARD = 0;
+
+    const CARDS = [
+        'A' => 12,
+        'K' => 11,
+        'Q' => 10,
+        'J' => 9,
+        'T' => 8,
+        9 => 7,
+        8 => 6,
+        7 => 5,
+        6 => 4,
+        5 => 3,
+        4 => 2,
+        3 => 1,
+        2 => 0,
+    ];
+
+    /**
+     * @param array<string|int> $cards
+     */
+    private function __construct(
+        public readonly array $cards,
+        public readonly string $bid
+    ) {
+    }
+
+    public static function fromString(string $line): self
+    {
+        [$cards, $bid] = explode(' ', $line);
+
+        return new self(str_split($cards), $bid);
+    }
+
+    public function beats(self $other): bool
+    {
+        return match ($this->type() <=> $other->type()) {
+            1 => true,
+            -1 => false,
+            0 => $this->tiebreaker($other),
+        };
+    }
+
+    private function tiebreaker(self $other): bool
+    {
+        for ($i = 0; $i < count($this->cards); $i++) {
+            $a = $this->cards[$i];
+            $b = $other->cards[$i];
+
+            if ($a === $b) {
+                continue;
+            }
+
+            return self::CARDS[$a] > self::CARDS[$b];
+        }
+
+        return false;
+    }
+
+    private function type(): int
+    {
+        $counts = array_count_values($this->cards);
+
+        sort($counts);
+
+        return match ($counts) {
+            [5] => self::FIVE_OF_A_KIND,
+            [1, 4] => self::FOUR_OF_A_KIND,
+            [2, 3] => self::FULL_HOUSE,
+            [1, 1, 3] => self::THREE_OF_A_KIND,
+            [1, 2, 2] => self::TWO_PAIR,
+            [1, 1, 1, 2] => self::ONE_PAIR,
+            default => self::HIGH_CARD,
+        };
+    }
+}
