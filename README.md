804e08807f973a2f18ff0de5357332a3039f18b2
Aaron Lord <hire@aaron.codes>
2023-12-29 02:06:03 +0000

    Day 5, Part 2

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 - Day 2 [Part 1](https://github.com/aaronlord/aoc/blob/3cb845c78fe00bd9bee0c7ac49e9232dfbc32706/app/Console/Commands/Day2Command.php), [Part 2](app/Console/Commands/Day2Command.php)
 - Day 3 [Part 1](https://github.com/aaronlord/aoc/blob/f7714d465e7009539b0e7eb36922e0a6ab7081d6/app/Console/Commands/Day3Command.php), [Part 2](app/Console/Commands/Day3Command.php)
 - Day 4 [Part 1](https://github.com/aaronlord/aoc/blob/88b8066e6e427586aba31436ac9aa8faa1414f37/app/Console/Commands/Day4Command.php), [Part 2](app/Console/Commands/Day4Command.php)
-- Day 5 [Part 1](app/Console/Commands/Day5Command.php)
+- Day 5 [Part 1](https://github.com/aaronlord/aoc/blob/ccbc964f14ebe2bf2b1d83bd580553be46fffdd1/app/Console/Commands/Day5Command.php), [Part 2](app/Console/Commands/Day5Command.php)
--- app/Console/Commands/Day5Command.php
+++ app/Console/Commands/Day5Command.php
@@ -3,11 +3,11 @@
 namespace App\Console\Commands;
 
 use App\Console\Commands\Day5\Map;
+use App\Console\Commands\Day5\Range;
 use App\Console\Commands\Day5\Seeds;
 use Illuminate\Console\Command;
 use Illuminate\Contracts\Filesystem\Filesystem;
-use Illuminate\Support\Facades\Pipeline;
-use Illuminate\Support\Str;
+use Illuminate\Support\Collection;
 
 class Day5Command extends Command
 {
@@ -26,54 +26,85 @@ class Day5Command extends Command
             ->reject(fn (string $line): bool => empty($line))
             ->map(fn (string $chunk): Map => Map::fromString($chunk));
 
-        $seeds = Pipeline::send($seeds)
-            ->through($maps->toArray())
-            ->then(fn (Seeds $seeds) => $seeds);
+        $this->line($this->min($seeds, $maps));
+    }
 
-        $this->line($seeds->min());
+    /**
+     * @param Collection<Map> $maps
+     */
+    public function min(Map $current, Collection $maps): int
+    {
+        if ($maps->isEmpty()) {
+            return min(array_column($current->ranges, 'sourceStart'));
+        }
+
+        $next = $maps->shift();
+
+        $toRec = [];
+
+        foreach ($current->ranges as $i => $currentRange) {
+            foreach ($next->ranges as $nextRange) {
+                if ($currentRange->sourceStart > $nextRange->sourceEnd) {
+                    continue;
+                }
+
+                $newRange = new Range(
+                    sourceStart: $nextRange->next(
+                        $currentRange->sourceStart > $nextRange->sourceStart
+                            ? $currentRange->sourceStart
+                            : $nextRange->sourceStart
+                    ),
+                    sourceEnd: $nextRange->next(
+                        $currentRange->sourceEnd > $nextRange->sourceEnd
+                            ? $nextRange->sourceEnd
+                            : $currentRange->sourceEnd
+                    ),
+                );
+
+                $toRec[$i][] = $newRange;
+
+                if ($currentRange->sourceEnd < $nextRange->sourceEnd) {
+                    break;
+                }
+            }
+
+            if (empty($toRec[$i])) {
+                $toRec[$i][] = clone $currentRange;
+            }
+        }
+
+        $toRec = new Map(array_merge(...$toRec));
+
+        return $this->min($toRec, $maps);
     }
 }
 
 namespace App\Console\Commands\Day5;
 
 use Illuminate\Support\Str;
-use Closure;
 
-class Seeds
+class Seeds extends Map
 {
-    /**
-     * @param array<int> $items
-     */
-    private function __construct(
-        public readonly array $items = []
-    ) {
-    }
-
     public static function fromString(string $string): self
     {
         return new self(array_map(
-            fn (string $string) => Seed::fromString($string),
-            array_filter(explode(' ', Str::after($string, ':')))
+            fn (array $array) => Seed::fromArray($array),
+            array_chunk(array_filter(explode(' ', Str::after($string, ':'))), 2)
         ));
-    }
-
-    public function min(): int
-    {
-        return min(array_column($this->items, 'value'));
     }
 }
 
-class Seed
+class Seed extends Range
 {
-    private function __construct(
-        public readonly int $number,
-        public int $value
-    ) {
-    }
-
-    public static function fromString(string $string): self
+    /**
+     * @param array<int> $array
+     */
+    public static function fromArray(array $array): self
     {
-        return new self((int) $string, (int) $string);
+        return new self(
+            sourceStart: $array[0],
+            sourceEnd: $array[0] + $array[1] - 1,
+        );
     }
 }
 
@@ -82,7 +113,7 @@ class Map
     /**
      * @param array<Range> $ranges
      */
-    private function __construct(
+    public function __construct(
         public readonly array $ranges = []
     ) {
     }
@@ -91,40 +122,53 @@ class Map
     {
         $lines = array_slice(array_filter(explode("\n", $string)), 1);
 
-        return new self(array_map(fn (string $line) => Range::fromString($line), $lines));
-    }
+        $ranges = array_map(fn (string $line) => Range::fromString($line), $lines);
 
-    /**
-     * @param Closure(Seeds): Seeds $next
-     */
-    public function handle(Seeds $seeds, Closure $next): Seeds
-    {
-        foreach ($seeds->items as $seed) {
-            $seed->value = $this->next($seed->value);
+        usort($ranges, function($a, $b) {
+            return $a->sourceStart <=> $b->sourceStart;
+        });
+
+        // Fill in the first 'missing range'
+        if ($ranges[0]->sourceStart > 0) {
+            $ranges = array_merge([
+                new Range(
+                    sourceStart: 0,
+                    sourceEnd: $ranges[0]->sourceStart - 1,
+                    destinationStart: 0,
+                    destinationEnd: $ranges[0]->sourceStart - 1,
+                )
+            ], $ranges);
         }
 
-        return $next($seeds);
-    }
+        // Fill in the 'missing ranges'
+        for ($i = 0; $i < count($ranges) - 1; $i++) {
+            $current = $ranges[$i];
+            $next = $ranges[$i + 1];
 
-    private function next(int $number): int
-    {
-        foreach ($this->ranges as $range) {
-            if ($number >= $range->sourceStart && $number <= $range->sourceEnd) {
-                return $range->destinationStart + ($number - $range->sourceStart);
+            if ($ranges[$i]->sourceEnd + 1 < $ranges[$i + 1]->sourceStart) {
+                $start = $ranges[$i]->sourceEnd + 1;
+                $end = $ranges[$i + 1]->sourceStart - 1;
+
+                $ranges[] = new Range(
+                    sourceStart: $start,
+                    sourceEnd: $end,
+                    destinationStart: $start,
+                    destinationEnd: $end,
+                );
             }
         }
 
-        return $number;
+        return new self($ranges);
     }
 }
 
 class Range
 {
-    private function __construct(
+    public function __construct(
         public readonly int $sourceStart,
         public readonly int $sourceEnd,
-        public readonly int $destinationStart,
-        public readonly int $destinationEnd
+        public readonly int $destinationStart = -1,
+        public readonly int $destinationEnd = -1,
     ) {
     }
 
@@ -132,11 +176,22 @@ class Range
     {
         [$destination, $source, $length] = explode(' ', $line);
 
+        $length -= 1;
+
         return new self(
             sourceStart: $source,
-            sourceEnd: $source + $length - 1,
+            sourceEnd: $source + $length,
             destinationStart: $destination,
-            destinationEnd: $destination + $length - 1,
+            destinationEnd: $destination + $length,
         );
+    }
+
+    public function next(int $number): int
+    {
+        if ($number >= $this->sourceStart && $number <= $this->sourceEnd) {
+            return $this->destinationStart + ($number - $this->sourceStart);
+        }
+
+        return $number;
     }
 }
