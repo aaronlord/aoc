37f7a54ab397247de2ec206ac87fe00574634c21
Aaron Lord <hire@aaron.codes>
2024-12-05 22:41:47 +0000

    day 1, part 1

--- app/Aoc/Day1/Solution.php
+++ app/Aoc/Day1/Solution.php
@@ -5,13 +5,27 @@ declare(strict_types=1);
 namespace App\Aoc\Day1;
 
 use App\Aoc\SolutionInterface;
+use Illuminate\Support\Facades\File;
 
 class Solution implements SolutionInterface
 {
     public function run(string $inputPath): int
     {
-        dd($inputPath);
+        $left = $right = [];
 
-        return 0;
+        foreach (File::lines($inputPath)->filter() as $line) {
+            [$left[], $right[]] = explode('   ', $line);
+        }
+
+        sort($left);
+        sort($right);
+
+        $sum = 0;
+
+        for ($i = 0; $i < count($left); $i++) {
+            $sum += abs($left[$i] - $right[$i]);
+        }
+
+        return $sum;
     }
 }
